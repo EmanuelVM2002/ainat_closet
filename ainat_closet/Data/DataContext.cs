138d1e01e01d174f16ee75f69f609a530510987c
EMANUEL VILLADA MU�ETON <emanuelvillada302298@correo.itm.edu.co>
2022-06-13 18:44:06 -0500

    Add project files.

---
+++ ainat_closet/Data/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using ainat_closet.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ainat_closet.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Country> Countries { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Country>().HasIndex(c => c.Name).IsUnique();
+        }
+
+    }
+}
---
+++ ainat_closet/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ainat_closet.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
