e3d386693111fe5c31aee1fb6757cd8736344f51
EMANUEL VILLADA MU�ETON <emanuelvillada302298@correo.itm.edu.co>
2022-06-16 10:44:33 -0500

    Yeih

---
+++ ainat_closet/Models/ProductsHomeViewModel.cs
@@ -0,0 +1,13 @@
+﻿using ainat_closet.Data.Entities;
+
+namespace ainat_closet.Models
+{
+    public class ProductsHomeViewModel
+    {
+        public Product Product1 { get; set; }
+        public Product Product2 { get; set; }
+        public Product Product3 { get; set; }
+        public Product Product4 { get; set; }
+    }
+
+}
